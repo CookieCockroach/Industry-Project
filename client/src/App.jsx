c1644c0b9b62fc909cd33498a6b048574fac2ec5
Kylie Chen <zqchen115@outlook.com>
2024-05-31 23:57:57 +0800

    merge develop into branch

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,18 @@
-<<<<<<< HEAD
-import './App.scss'
-import Tracker from './pages/Tracker/Tracker'
-=======
 import "./App.scss";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import QuizPage from "./pages/QuizBody/QuizBody";
 import Categories from "./pages/Category/Category";
->>>>>>> develop
+import Tracker from "./pages/Tracker/Tracker";
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-    <Tracker></Tracker>
-    </>
-  )
-=======
     <BrowserRouter>
       <Routes>
         <Route exact path="/" element={<Categories />} />
         <Route path="/quiz" element={<QuizPage />} />
+        <Route path="/tracker" element={<Tracker />} />
       </Routes>
     </BrowserRouter>
   );
->>>>>>> develop
 }
 export default App;
