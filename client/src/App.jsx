11c6f8f3e173a8e9be422f236bcfe53aeaa68f1f
Rinu2024 <rinu.work@gmail.com>
2024-05-31 11:53:38 -0400

    Added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,15 @@
-import './App.css'
+import './App.scss'
+import React from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Tracker from './pages/Tracker/Tracker'
 
 function App() {
-
   return (
-    <>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/track" element={<Tracker />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
